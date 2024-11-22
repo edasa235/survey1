faa5f7a938d8bdc9fdfa779b9bbedbff7a4b808b
edasa235 <edvardaslaksen130206@gmail.com>
2024-11-22 14:21:03 +0100

    fix db.js

--- backend/api/db.js
+++ backend/api/db.js
@@ -4,18 +4,19 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const { Pool } = pkg;
-
 const pool = new Pool({
   host: process.env.PGHOST,
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
-  port: parseInt(process.env.DB_PORT, 10) || 5432,
-  ssl: { rejectUnauthorized: false },
+  port: parseInt(process.env.PGPORT, 10) || 5432,
+  ssl: { rejectUnauthorized: false }, // Allow insecure SSL
 });
 
 export async function getConnection() {
   return pool.connect();
 }
+
+console.log(pool)
 
 export default pool;
