5b287c0c93f20e073f9406098bbb152333861799
edasa235 <edvardaslaksen130206@gmail.com>
2024-11-25 12:45:29 +0100

    fix answer.js

--- backend/api/answers.js
+++ backend/api/answers.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { getConnection } from './db.js';
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
@@ -8,7 +9,13 @@ router.post('/', async (req, res) => {
 	res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-	const { responses, user_id } = req.body;
+	let { responses, user_id } = req.body;
+
+	// Generate a new UUID if user_id is not provided
+	if (!user_id) {
+		user_id = uuidv4();
+	}
+
 	const client = await getConnection(); // Fetch connection once
 	try {
 		// Loop through the responses object and insert each answer into the database
@@ -20,7 +27,7 @@ router.post('/', async (req, res) => {
 			);
 		}
 
-		res.status(201).json({ message: 'Answers stored successfully' });
+		res.status(201).json({ message: 'Answers stored successfully', user_id });
 	} catch (error) {
 		console.error('Error storing answers:', error);
 		res.status(500).json({ error: 'Failed to store answers' });
